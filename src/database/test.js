395e8e6aac83940112c8e2d8831630329c7987a4
Lucio Nonato <luciononato@hotmail.com>
2020-08-07 19:35:17 -0300

    5.1 commit
    projeto versão 1 finalizado

--- src/database/test.js
+++ src/database/test.js
@@ -11,7 +11,7 @@ Database.then(async (db) => {
     }    
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "20", 
     }
 
@@ -28,6 +28,26 @@ Database.then(async (db) => {
         }
     ]
 
-    await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
+    const selectedProffy = await db.all("SELECT * FROM proffys")
+
+    const selectClassesAndPoffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1; 
+    `)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    console.log(selectClassesSchedules)
+    
 })
